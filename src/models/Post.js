ba6342157aade4b330f9b0c67c1e723bd6fa9da4
Jairo H <1992jairoh@gmail.com>
2025-12-24 18:47:42 -0500

    feat: Agregar campo game al modelo Post para filtrado por juego

--- src/models/Post.js
+++ src/models/Post.js
@@ -5,6 +5,7 @@ const PostSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
   category: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  game: { type: mongoose.Schema.Types.ObjectId, ref: 'Game', required: true, index: true },
   author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   images: [{ type: String }],
   createdAt: { type: Date, default: Date.now },
