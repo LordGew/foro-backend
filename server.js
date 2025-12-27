90d7a9fd726f9eafb18ea4650ba8627a87aaf763
Jairo H <1992jairoh@gmail.com>
2025-12-26 19:01:31 -0500

    Agregar middleware anti-caché global en servidor backend

--- server.js
+++ server.js
@@ -95,6 +95,17 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
+// Middleware anti-caché para todas las respuestas
+app.use((req, res, next) => {
+  res.set({
+    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+    'Pragma': 'no-cache',
+    'Expires': '0',
+    'Surrogate-Control': 'no-store'
+  });
+  next();
+});
+
 // Parser y cookies
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
